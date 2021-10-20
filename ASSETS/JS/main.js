bdc42daa1804b5b89c06563c47c99ce288060afc
gbelda <gbelda56@gmail.com>
2021-10-20 15:10:01 +0200

    Created HTML,CSS and JS file structure

---
+++ ASSETS/JS/main.js
@@ -0,0 +1,11 @@
+//Chiedere all'utente un livello di difficolta
+//generare una griglia a seconda del livello 
+//difficoltà 1 => tra 1 e 100
+//difficoltà 2 => tra 1 e 81
+// difficoltà 3 => tra 1 e 49
+//Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+
+//Usare prompt per chiedere il livello di difficolta
+//creare funzione con loop per stabilire il numero di celle
+//collegare il prompt con il loop utilizzando il condizionale
+//Assegnare un event listener a una cella utilizzando "This"
